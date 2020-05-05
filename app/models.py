1a4305a78cff5f3e5674d3d32fcd2d7938c17e62
NoriKaneshige <atsunori@umich.edu>
2020-05-05 16:40:20 -0400

    first commit

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=200, unique=True)
+
+	# def __str__(self):
+	#     return self.file.url
+
+	def __str__(self):
+		return self.title
